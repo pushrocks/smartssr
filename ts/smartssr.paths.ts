637170ecd2376e6a71349c966daa500514254371
Phil Kunz <gitlab@philkunz.com>
2020-02-12 21:38:04 +0000

    fix(core): update

--- ts/smartssr.paths.ts
+++ ts/smartssr.paths.ts
@@ -2,3 +2,4 @@ import * as plugins from './smartssr.plugins';
 
 export const packageDir = plugins.path.join(__dirname, '../');
 export const noGitDir = plugins.path.join(packageDir, './.nogit');
+plugins.smartfile.fs.ensureDirSync(noGitDir);
--- ts/smartssr.plugins.ts
+++ ts/smartssr.plugins.ts
@@ -1,8 +1,7 @@
 // node native
 import * as path from 'path';
-import * as fs from 'fs';
 
-export { path, fs };
+export { path };
 
 // @pushrocks scope
 import * as smartdelay from '@pushrocks/smartdelay';
