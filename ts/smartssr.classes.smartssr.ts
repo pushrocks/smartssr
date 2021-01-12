46a6c903e194435a15e0b5f7d42f2699bfcf2198
Phil Kunz <gitlab@philkunz.com>
2021-01-12 15:17:31 +0000

    fix(core): update

--- ts/smartssr.classes.smartssr.ts
+++ ts/smartssr.classes.smartssr.ts
@@ -51,7 +51,7 @@ export class SmartSSR {
     renderTimeMeasurement.start();
     await page.goto(urlArg, {
       waitUntil: 'networkidle2',
-      timeout: 0
+      timeout: 30000
     });
 
     let screenshotBuffer: Buffer;
